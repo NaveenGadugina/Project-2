6ba5e7797c658d6d793d52e4966b31d57a633619
Prajwal-031 <mpprajwal11@gmail.com>
2025-05-29 00:13:17 +0530

    update the code syntax

--- Cprogram.c
+++ Cprogram.c
@@ -1,4 +1,4 @@
-Cprogram.c#include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
@@ -28,7 +28,7 @@ int main(){
             printf("You selected Circular Singly Linked List.\n");
             scll();
             break;
-        default:
+	default:
             printf("Invalid option. Please select a valid option.\n");
     }
 }
